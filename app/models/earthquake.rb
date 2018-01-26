73a5a8f46c2262710dcf95bb19d64111ebb0c338
Tod Kaiser <todkaiser@gmail.com>
2018-01-26 01:49:32 -0800

    Update annotations

--- app/models/earthquake.rb
+++ app/models/earthquake.rb
@@ -10,6 +10,7 @@
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  time        :datetime
 #
 
 class Earthquake < ApplicationRecord
--- spec/models/earthquake_spec.rb
+++ spec/models/earthquake_spec.rb
@@ -3,13 +3,14 @@
 # Table name: earthquakes
 #
 #  id          :integer          not null, primary key
-#  magnitude   :decimal(5, )
+#  magnitude   :decimal(2, )
 #  longitude   :string
 #  latitude    :string
 #  region_type :string
 #  title       :string
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  time        :datetime
 #
 
 require 'rails_helper'
