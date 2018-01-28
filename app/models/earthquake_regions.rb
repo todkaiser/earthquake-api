48181b0536ca7fa24c93bb5eaed2bb1d5f8b941c
Tod Kaiser <todkaiser@gmail.com>
2018-01-28 15:09:54 -0800

    Set default region_code

--- app/models/earthquake_regions.rb
+++ app/models/earthquake_regions.rb
@@ -2,6 +2,7 @@ class EarthquakeRegions
   DEFAULT_REGION_COUNT = 10
   DEFAULT_DAYS_BACK = 30
   DEFAULT_REGION_TYPE = 'country'.freeze
+  DEFAULT_REGION_CODE = 'country_code'.freeze
 
   def initialize(count:, days:, region_type:, region_code:)
     @count = filter_count_param(count)
@@ -55,6 +56,8 @@ class EarthquakeRegions
       'country_code'
     when 'state'
       'state_code'
+    else
+      DEFAULT_REGION_CODE
     end
   end
 end
