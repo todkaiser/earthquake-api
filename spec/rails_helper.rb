7a4d259cb7872ec2b78bfb24a73e514242963aa4
Tod Kaiser <todkaiser@gmail.com>
2018-01-28 16:19:06 -0800

    Clean up spec config helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,32 +1,10 @@
-# require database cleaner at the top level
-require 'database_cleaner'
+require 'spec_helper'
 
-# [...]
-# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
-    with.library :rails
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
   end
-end
-
-# [...]
-RSpec.configuration do |config|
-  # [...]
-  # add `FactoryGirl` methods
-  config.include FactoryGirl::Syntax::Methods
-
-  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  # start the transaction strategy as examples are run
-  config.around do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
-  # [...]
 end
