f84acb1a0847a7d51749dca578461ad595277714
Tod Kaiser <todkaiser@gmail.com>
2018-01-28 16:49:20 -0800

    Add model spec for earthquakes

--- spec/models/earthquake_spec.rb
+++ spec/models/earthquake_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Earthquake, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Earthquake, type: :model do
+  subject { described_class.new }
+
+  before { allow_any_instance_of(described_class).to receive(:reverse_geocode).and_return(nil) }
+
+  it 'is valid with valid attributes' do
+    subject.magnitude = 0.98
+    subject.longitude = -122.5268333
+    subject.latitude = 37.7015
+    subject.tsunami = 0
+    subject.title = 'M 1.0 - 4km SSW of San Francisco Zoo, CA'
+    subject.time = Time.at(1_517_004_553_160 / 1000).utc.to_datetime
+
+    expect(subject).to be_valid
+  end
 end
