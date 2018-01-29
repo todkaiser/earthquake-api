32cc149a325500b7582c320541d4b0b9622a6cd6
Tod Kaiser <todkaiser@gmail.com>
2018-01-28 19:41:38 -0800

    Create factory for earthquakes

---
+++ spec/factories/earthquakes.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :earthquake do
+    address      { Faker::Address.street_address }
+    latitude     { Faker::Address.latitude }
+    longitude    { Faker::Address.longitude }
+    magnitude    { Faker::Number.decimal(1, 2) }
+    time         { Faker::Time.between(DateTime.now - 10, DateTime.now) }
+    tsunami      { Faker::Number.between(0, 1) }
+  end
+end
