28566f2b9214400fadb44861770979edd6452997
Tod Kaiser <todkaiser@gmail.com>
2018-01-28 19:26:29 -0800

    Add databasecleaner config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,43 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'faker'
+
+Dir[
+  Rails.root.join('spec', 'support', '**', '*.rb')
+].each { |file| require file }
 
 RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.order = :random
+  config.use_transactional_fixtures = false
+
+  config.include FactoryBot::Syntax::Methods
+  config.include Rails.application.routes.url_helpers
+
+  config.infer_spec_type_from_file_location!
+
+  config.mock_with :rspec do |c|
+    c.syntax = %i[should expect]
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before do
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 end
